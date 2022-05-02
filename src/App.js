f364a3726086c3182cadd0050e07b6c9011a808d
andrey678 <drew6496@yandex.ru>
2022-05-03 00:34:45 +0300

    Add component Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import Header from './Header/Header';
+import Footer from './Footer/Footer';
 
 class App extends React.Component {
   constructor(props) {
@@ -67,60 +68,8 @@ class App extends React.Component {
               <a className="btn btn-primary btn-sm">OK</a></div>
           </div>
         </div>
+        <Footer />
 
-        <footer id="footer" className="footer">
-          <div className="footer-bottom">
-            <div className="container">
-              <div className="flex-container">
-                <div className="flex-item">
-                  <h1 className="footer-title"><a href="https://itgid.info">2022 &copy; Currency Exchange Rate</a></h1>
-                  <p>All Rights Reserved</p>
-
-                  <div>
-                    <div className="flex-item">
-                      <div className="module-body">
-                        <ul className="list-unstyled">
-                          <li><a href="/sitemap/" tooltip="Карта сайта">Карта сайта</a></li>
-                          <li><a href="/sitemaps.xml" target="_blank" tooltip="Google sitemap">Google Sitemap</a></li>
-                        </ul>
-                        <div>
-                        </div>
-                        <div className="flex-item">
-                          <div className="module-body">
-                            <ul className="list-unstyled">
-                              <li><a href="/contact/" tooltip="Контакты">Контакты</a></li>
-                              <li><a href="/d">Гарантии</a></li>
-                              <li><a href="/s">О сервисе</a></li>
-                              <li><a href="/o">Условия возврата</a></li>
-                              <li><a href="/p">Соглашение о использовании сервиса</a></li>
-                            </ul>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="copyright-bar">
-                    <div className="flex-container">
-                      <div className="flex-item">
-                        <ul className="link">
-                        </ul>
-                      </div>
-                      <div className="flex-item">
-                        <div className="clearfix payment-methods">
-                          <ul>
-                            <img src="/skin/default/images/dummy.png" data-toggle="tooltip" data-placement="top"
-                              data-original-title="www.webmoney.ru" alt="" />
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </footer>
       </div>
     );
   }
